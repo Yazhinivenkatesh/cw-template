98eed1b0e3ac973f6049c6dd3995c84840043b7b
Yazhinivenkatesh <yazhinivenkatesh2000@gmail.com>
2022-05-25 12:43:12 +0530

    Added initial config

---
+++ src/AddReferral.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { coin, SigningCosmosClient } from '@cosmjs/launchpad'
+// import {
+//     SigningCosmWasmClient,
+// } from '@cosmjs/cosmwasm-stargate';
+// import { Tendermint34Client } from "@cosmjs/tendermint-rpc";
+
+
+
+const AddReferral = () => {
+    return(
+        <p>Add-Referral</p>
+    );
+}
+export default AddReferral;
